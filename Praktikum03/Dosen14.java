0893f3c94606ff488948f31cfd7697b900e1b75d
Furqon Adzikri <furqonadzikri311@gmail.com>
2026-03-05 21:46:19 +0700

    Jawaban Tugas No.2 -Dosen

--- Praktikum03/Dosen14.java
+++ Praktikum03/Dosen14.java
@@ -1,16 +1,22 @@
-package Praktikum03;
+package praktikum03;
 
 public class Dosen14 {
-
     String kode;
     String nama;
     boolean jenisKelamin;
     int usia;
 
-    public Dosen14(String kode, String nama, boolean jenisKelamin, int usia) {
+    public Dosen14(String kode, String nama, boolean jenisKelamin, int usia){
         this.kode = kode;
         this.nama = nama;
         this.jenisKelamin = jenisKelamin;
         this.usia = usia;
+    }
+
+    public void tampilInformasi(){
+        System.out.println("Kode : " + kode);
+        System.out.println("Nama : " + nama);
+        System.out.println("Jenis Kelamin : " + (jenisKelamin ? "Pria" : "Wanita"));
+        System.out.println("Usia : " + usia);
     }
 }

a3f179890bc56d9372891e9b3eb3b86461988b00
Furqon Adzikri <furqonadzikri311@gmail.com>
2026-03-05 21:20:18 +0700

    Jawaban Pertanyaan no 2&3 Percobaan 3.4.1 -menambahkan method

--- Praktikum03/MataKuliah14.java
+++ Praktikum03/MataKuliah14.java
@@ -6,11 +6,21 @@ public class MataKuliah14 {
     public int sks;
     public int jumlahJam;
 
-    // Constructor berparameter
-    public MataKuliah14(String kode, String nama, int sks, int jumlahJam) {
+    public MataKuliah14() {
+    }
+
+    public void tambahData(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    public void cetakInfo() {
+        System.out.println("Kode       : " + kode);
+        System.out.println("Nama       : " + nama);
+        System.out.println("SKS        : " + sks);
+        System.out.println("Jumlah Jam : " + jumlahJam);
+        System.out.println("---------------------------");
     }
 }
